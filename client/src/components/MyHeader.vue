8b8cc6ff749133964ed3cac8b3479f07d2460ba6
FerleyAriasSena <feareas@gmail.com>
2021-10-20 15:59:27 -0500

    add header

---
+++ client/src/components/MyHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="header">
+    <h1>Health Reader</h1>
+    <div class="burger" v-if="burgerSwitch">
+      <button @click="()=> burgerSwitch= !burgerSwitch" class="burger__switch">
+        <img :src="require('@/assets/icons/burgerIcon.svg')" alt="">
+      </button>
+      <div :class="burgerSwitch && 'burger__background--active'" class="burger__background"/>
+      <nav class="burger__menu" :class="burgerSwitch && 'burger__menu--active'">
+        <router-link to="" class="btn btn--menu">
+          Escanear
+        </router-link>
+        <router-link to="" class="btn btn--menu">
+          Ver Historias
+        </router-link>
+        <router-link to="" class="btn btn--menu">
+          Ver Usuarios
+        </router-link>
+        <router-link to="" class="btn btn--menu">
+          Salir
+        </router-link>
+      </nav>
+    </div>
+  </header> 
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+import {useRoute} from 'vue-router'
+export default {
+  setup() {
+    const burgerSwitch = ref(false)
+    const burgerShow = ref(false)
+    const Route = useRoute()
+    watch(Route, ()=> {
+      if(!Route.meta.requireAuth) {
+        burgerSwitch.value = false
+      } else {
+        burgerSwitch.value = true
+      }
+    })
+    return {
+      burgerSwitch,
+      burgerShow
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@/css/components/header.scss';
+</style>
