e38193a5b09832b7e5208912541271b0cc3ebc28
DarienJ <djromero905@misena.edu.co>
2021-10-22 14:17:50 -0500

    bug fixes

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,7 +3,7 @@ import {Router} from 'express';
 import {check} from 'express-validator';
 import usuarioControllers from '../controllers/usuario.js';
 import { validarCampo } from '../middlewares/validarCampos.js';
-import { validarJWR } from '../middlewares/validarJwt.js';
+import { validarJWR } from '../middlewares/validarJWT.js';
 import { validarRol } from '../middlewares/validarRoles.js';
 import { validarExistenciaArchivo } from '../middlewares/validarArchivo.js';
 
